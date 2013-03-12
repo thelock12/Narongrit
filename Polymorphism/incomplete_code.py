c3023675ab6401d7a4352bdc2fd345f3c3ea3d15
Anuruth <rose.0787@hotmail.com>
2013-03-12 19:45:47 +0700

    Update incomplete_code.py

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -27,6 +27,13 @@ class Taxi( Transportation ):
 
    def find_cost(self):
       return self.distance * 40
+class Train( Transportation ):
+
+   def __init__(self, start, end, distance):
+      Transportation.__init__(self, start, end, distance)
+
+   def find_cost(self):
+      return self.distance * 5
 
    
 # main program
