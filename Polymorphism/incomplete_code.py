7451900bade819cd11cee9c7525b3334a45adbfd
NathanSanxez <feel_rebirthz@hotmail.com>
2013-03-12 19:53:42 +0700

    Update incomplete_code.py
    add main class

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -38,14 +38,14 @@ class Train( Transportation ):
       return self.station * 5
 
    
-# main program
+def main():
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
